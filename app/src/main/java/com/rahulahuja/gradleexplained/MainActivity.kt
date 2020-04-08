2e47c1251d433dfc7f08e0d0871bfe9478f02eaa
ahujarahul <ahuja.rahul05@gmail.com>
2020-04-08 17:08:33 +0530

    Gradle config added with multiple flavors and build types. Also, source basis flavors added for app module.

---
+++ app/src/main/java/com/rahulahuja/gradleexplained/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.rahulahuja.gradleexplained
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        btn_next.setOnClickListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            btn_next.id -> {
+                if (BuildConfig.APPLICATION_ID.contains(resources.getString(R.string.paid))) {
+                    startActivity(Intent("flavor.paid.PaidActivity"))
+                } else {
+                    Toast.makeText(this, resources.getString(R.string.upgrade_to_paid), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/paid/java/com/rahulahuja/gradleexplained/paid/PaidActivity.kt
@@ -0,0 +1,13 @@
+package com.rahulahuja.gradleexplained.paid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rahulahuja.gradleexplained.R
+
+class PaidActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paid)
+    }
+}
