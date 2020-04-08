fbe0565204077b3b09c97dbb7d8f45cbc503b23c
ahujarahul <ahuja.rahul05@gmail.com>
2020-04-08 19:26:07 +0530

    1. creating Java/ Kotlin module 2. providing it as dependency to app module

---
+++ KotlinJokes/src/main/java/com/rahulahuja/kotlinjokes/Joker.kt
@@ -0,0 +1,8 @@
+package com.rahulahuja.kotlinjokes
+
+public class Joker() {
+
+    fun tellJoke(): String {
+        return "Haha.. I am not here to tell jokes. You are wasted."
+    }
+}
--- app/src/paid/java/com/rahulahuja/gradleexplained/paid/PaidActivity.kt
+++ app/src/paid/java/com/rahulahuja/gradleexplained/paid/PaidActivity.kt
@@ -3,11 +3,19 @@ package com.rahulahuja.gradleexplained.paid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rahulahuja.gradleexplained.R
+import com.rahulahuja.kotlinjokes.Joker
+import kotlinx.android.synthetic.paid.activity_paid.*
 
 class PaidActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paid)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        tv_joke.text = Joker().tellJoke()
     }
 }
